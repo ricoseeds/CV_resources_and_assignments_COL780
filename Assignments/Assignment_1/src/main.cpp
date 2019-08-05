b2ae6edb29708182c3b2d535474602efc9692eff
ricoseeds <rghchaks73@gmail.com>
2019-08-06 02:21:09 +0530

    Restructure

--- Assignments/Assignment_1/src/main.cpp
+++ Assignments/Assignment_1/src/main.cpp
@@ -1,224 +1,108 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <string>
+
+#include "json.hpp"
+
+// cv headers
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
+
 using namespace cv;
 using namespace std;
+using json = nlohmann::json;
 
+typedef enum
+{
+    gray_to_rgb,
+    rgb_to_gray
+} ColorSpace;
+typedef enum
+{
+    opening = 2
+} MorphType;
+
+static int IMG_HEIGHT, IMG_WIDTH;
+
+//functions
+void convert_to_gray_scale(Mat &, Mat &, ColorSpace);
+void show_split_window(Mat &, Mat &, Mat &);
 int main(int argc, char **argv)
 {
-    const int IMG_HEIGHT = 480, IMG_WIDTH = 640;
-    int morph_elem = 0;     // 0
-    int morph_size = 1;     // 3
-    int morph_operator = 0; // 0
-    int lowThreshold = 100; // 100
-    const int ratio = 3;
-    const int kernel_size = 3;
-    Mat detected_edges, dst;
-    //create Background Subtractor objects
     Ptr<BackgroundSubtractor> pBackSub;
     pBackSub = createBackgroundSubtractorMOG2();
-    // pBackSub = createBackgroundSubtractorKNN();
+    std::ifstream ifile("Assignments/Assignment_1/input/tuning_params.json");
+    json j;
+    ifile >> j;
+    // declarations
+    const string filename = j["data"];
+    // VideoCapture capture(filename);
     VideoCapture capture("data/videos/1.mp4");
     if (!capture.isOpened())
     {
-        //error in opening the video input
         cerr << "Unable to open video file" << endl;
         return 0;
     }
-    Mat frame, fgMask, fgMask_col;
-    Mat win_mat(cv::Size(IMG_WIDTH * 2, IMG_HEIGHT), CV_8UC3);
+    IMG_HEIGHT = j["resize_height"];
+    IMG_WIDTH = j["resize_width"];
+    Mat frame, frame_bw, mog2_mask;
+    Mat concatenated_window_frame(cv::Size(IMG_WIDTH * 2, IMG_HEIGHT), CV_8UC3);
+    int morph_elem = j["morph_element"];    // 0
+    int morph_size = j["morph_point_size"]; // 3
+    int operation = MorphType::opening;
+
     while (true)
     {
         capture >> frame;
         if (frame.empty())
             break;
-        // TODO: Histogram Equilization
-
-        //update the background model
-        pBackSub->apply(frame, fgMask);
-
-        // // Copy small images into big mat
-        cv::resize(frame, frame, cv::Size(IMG_WIDTH, IMG_HEIGHT));
-        cv::resize(fgMask, fgMask, cv::Size(IMG_WIDTH, IMG_HEIGHT));
-
-        //get the frame number and write it on the current frame
-        rectangle(frame, cv::Point(10, 2), cv::Point(100, 20), cv::Scalar(255, 255, 255), -1);
-        stringstream ss;
-        ss << capture.get(CAP_PROP_POS_FRAMES);
-        string frameNumberString = ss.str();
-        putText(frame, frameNumberString.c_str(), cv::Point(15, 15), FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-
-        //show the current frame and the fg masks
-        namedWindow("Display window", 0); // Create a window for display.
-
+        namedWindow("Display window", 0);
+        // resize frame optional
+        resize(frame, frame, Size(IMG_WIDTH, IMG_HEIGHT));
+        // convert to gray scale
+        convert_to_gray_scale(frame, frame_bw, rgb_to_gray);
+        // Histogram Equilization <NOT REQUIRED>
+        // equalizeHist(frame_bw, frame_bw);
+        // pBackSub->apply(frame_bw, fgMask);
+        pBackSub->apply(frame_bw, frame_bw);
         // opening morph operation on fgMask
-        int operation = morph_operator + 2;
+
         Mat element = getStructuringElement(morph_elem, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size)); // TUNE
-        morphologyEx(fgMask, fgMask, operation, element);
+        morphologyEx(frame_bw, frame_bw, operation, element);
 
-        //SHOW RESULT OF MORPH
-        cvtColor(fgMask, fgMask_col, cv::COLOR_GRAY2RGB);
-
-        // TODO: Use sobel with tuning on its kernel size
-
-        // TODO: Hough transform
-
-        // SAVE: Canny edge detection on (fgMask => GRAY_CHANNEL)
-        // dst.create(fgMask.size(), fgMask.type());
-        // blur(fgMask, detected_edges, Size(3, 3)); // TUNE
-        // Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
-        // dst = Scalar::all(0);
-        // fgMask.copyTo(dst, detected_edges);
-
-        // show operation : window
-        frame.copyTo(win_mat(cv::Rect(0, 0, IMG_WIDTH, IMG_HEIGHT)));
-        fgMask_col.copyTo(win_mat(cv::Rect(IMG_WIDTH, 0, IMG_WIDTH, IMG_HEIGHT)));
-        imshow("Display window", win_mat);
-
-        //get the input from the keyboard
+        //Thresholding to get rid of shadows
+        threshold(frame_bw, frame_bw, 230, 255, 0);
+        // Rendering stage
+        convert_to_gray_scale(frame_bw, frame_bw, gray_to_rgb); // mandatory step so that concatenated_window_frame has same color Channel
+        show_split_window(frame, frame_bw, concatenated_window_frame);
+        // show frame
+        imshow("Display window", concatenated_window_frame);
         int keyboard = waitKey(1); // ?
         if (keyboard == 'q' || keyboard == 27)
             break;
     }
-    return 0;
 }
 
-// #include "opencv2/imgproc.hpp"
-// #include "opencv2/imgcodecs.hpp"
-// #include "opencv2/highgui.hpp"
-// #include <iostream>
-// using namespace cv;
-// using namespace std;
-// int main(int argc, char **argv)
-// {
-//     cv::CommandLineParser parser(argc, argv,
-//                                  "{@input   |data/filtered_probe.png|input image}"
-//                                  "{ksize   k|1|ksize (hit 'K' to increase its value)}"
-//                                  "{scale   s|1|scale (hit 'S' to increase its value)}"
-//                                  "{delta   d|0|delta (hit 'D' to increase its value)}"
-//                                  "{help    h|false|show help message}");
-//     cout << "The sample uses Sobel or Scharr OpenCV functions for edge detection\n\n";
-//     parser.printMessage();
-//     cout << "\nPress 'ESC' to exit program.\nPress 'R' to reset values ( ksize will be -1 equal to Scharr function )";
-//     // First we declare the variables we are going to use
-//     Mat image, src, src_gray;
-//     Mat grad;
-//     const String window_name = "Sobel Demo - Simple Edge Detector";
-//     int ksize = parser.get<int>("ksize");
-//     int scale = parser.get<int>("scale");
-//     int delta = parser.get<int>("delta");
-//     int ddepth = CV_16S;
-//     String imageName = parser.get<String>("@input");
-//     // As usual we load our source image (src)
-//     image = imread(imageName, IMREAD_COLOR); // Load an image
-//     // Check if image is loaded fine
-//     if (image.empty())
-//     {
-//         printf("Error opening image: %s\n", imageName.c_str());
-//         return 1;
-//     }
-//     for (;;)
-//     {
-//         // Remove noise by blurring with a Gaussian filter ( kernel size = 3 )
-//         GaussianBlur(image, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
-//         // Convert the image to grayscale
-//         cvtColor(src, src_gray, COLOR_BGR2GRAY);
-//         Mat grad_x, grad_y;
-//         Mat abs_grad_x, abs_grad_y;
-//         Sobel(src_gray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
-//         Sobel(src_gray, grad_y, ddepth, 0, 1, ksize, scale, delta, BORDER_DEFAULT);
-//         // converting back to CV_8U
-//         convertScaleAbs(grad_x, abs_grad_x);
-//         convertScaleAbs(grad_y, abs_grad_y);
-//         addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-//         Canny(grad, grad, 50, 200, 3);
-//         imshow(window_name, grad);
-//         char key = (char)waitKey(0);
-//         if (key == 27)
-//         {
-//             return 0;
-//         }
-//         if (key == 'k' || key == 'K')
-//         {
-//             ksize = ksize < 30 ? ksize + 2 : -1;
-//         }
-//         if (key == 's' || key == 'S')
-//         {
-//             scale++;
-//         }
-//         if (key == 'd' || key == 'D')
-//         {
-//             delta++;
-//         }
-//         if (key == 'r' || key == 'R')
-//         {
-//             scale = 1;
-//             ksize = -1;
-//             delta = 0;
-//         }
-//     }
-//     return 0;
-// }
+void convert_to_gray_scale(Mat &frame, Mat &output_fr, ColorSpace c) // TODO : add all conversions for transforming color space
+{
+    switch (c)
+    {
+    case gray_to_rgb:
+        cvtColor(frame, output_fr, cv::COLOR_GRAY2RGB);
+        break;
+    case rgb_to_gray:
+        cvtColor(frame, output_fr, cv::COLOR_BGR2GRAY);
+        break;
+    }
+}
 
-// #include "opencv2/imgcodecs.hpp"
-// #include "opencv2/highgui.hpp"
-// #include "opencv2/imgproc.hpp"
-// using namespace cv;
-// using namespace std;
-// int main(int argc, char **argv)
-// {
-//     // Declare the output variables
-//     Mat dst, cdst, cdstP;
-//     const char *default_file = "data/sobel_canny.png";
-//     const char *filename = argc >= 2 ? argv[1] : default_file;
-//     // Loads an image
-//     Mat src = imread(filename, IMREAD_GRAYSCALE);
-//     // Check if image is loaded fine
-//     if (src.empty())
-//     {
-//         printf(" Error opening image\n");
-//         printf(" Program Arguments: [image_name -- default %s] \n", default_file);
-//         return -1;
-//     }
-//     // Edge detection
-//     Canny(src, dst, 10, 50, 3);
-//     // Copy edges to the images that will display the results in BGR
-//     cvtColor(dst, cdst, COLOR_GRAY2BGR);
-//     cdstP = cdst.clone();
-//     // Standard Hough Line Transform
-//     vector<Vec2f> lines;                               // will hold the results of the detection
-//     HoughLines(dst, lines, 1, CV_PI / 180, 255, 0, 0); // runs the actual detection
-//     // Draw the lines
-//     for (size_t i = 0; i < lines.size(); i++)
-//     {
-//         float rho = lines[i][0], theta = lines[i][1];
-//         Point pt1, pt2;
-//         double a = cos(theta), b = sin(theta);
-//         double x0 = a * rho, y0 = b * rho;
-//         pt1.x = cvRound(x0 + 1000 * (-b));
-//         pt1.y = cvRound(y0 + 1000 * (a));
-//         pt2.x = cvRound(x0 - 1000 * (-b));
-//         pt2.y = cvRound(y0 - 1000 * (a));
-//         // line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, LINE_AA);
-//     }
-//     // Probabilistic Line Transform
-//     // vector<Vec4i> linesP;                                 // will hold the results of the detection
-//     // HoughLinesP(dst, linesP, 1, CV_PI / 180, 50, 50, 10); // runs the actual detection
-//     // // Draw the lines
-//     // for (size_t i = 0; i < linesP.size(); i++)
-//     // {
-//     //     Vec4i l = linesP[i];
-//     //     line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
-//     // }
-//     // Show results
-//     imshow("Source", src);
-//     imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
-//     // imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
-//     // Wait and Exit
-//     waitKey();
-//     return 0;
-// }
+void show_split_window(Mat &m1, Mat &m2, Mat &ccat_frame)
+{
+    m1.copyTo(ccat_frame(Rect(0, 0, IMG_WIDTH, IMG_HEIGHT)));
+    m2.copyTo(ccat_frame(Rect(IMG_WIDTH, 0, IMG_WIDTH, IMG_HEIGHT)));
+    // imshow("Display window", ccat_frame);
+}

fe07fae1c166f28b3445890e3acaf0a0b6902b24
Sukhraj Singh <singh.sukhraj@gmail.com>
2019-08-05 23:39:46 +0530

    Fis EigenExample

--- Assignments/EigenExample/src/EigenExample.cpp
+++ Assignments/EigenExample/src/EigenExample.cpp
@@ -1,35 +1,38 @@
+// Current project
 #include "EigenExample.h"
+
+// OpenCV
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
-
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
-// Eigen includes
+// Eigen
 #include <Eigen/Dense>
 
 // STL includes
 #include <iostream>
+#include <string.h>
 
 using Eigen::MatrixXd;
 using namespace cv;
 using namespace std;
 
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-    char the_path[256];
+	Mat src = imread(argv[1], IMREAD_GRAYSCALE);
+	if (src.empty()) {
+		cout << "I Could not open or find the image!\n"
+			<< endl;
+		cout << "Usage: " << argv[0] << " <Input image>" << endl;
+		return -1;
+	}
 
-    getcwd(the_path, 255);
-    strcat(the_path, "/");
-    strcat(the_path, argv[0]);
+	namedWindow("Display window", WINDOW_AUTOSIZE);
 
-    printf("%s\n", the_path);
-    cv::Mat original = imread("data/lena.jpg", 0);
-    namedWindow("Display window", WINDOW_AUTOSIZE);
-    imshow("Display window", original);
+	imshow("Display window", src);
+	
     waitKey(0);
     return 0;
 }

0e81a62f9bff2c55632655c43cb835aa6ef4b8c3
Sukhraj Singh <singh.sukhraj@gmail.com>
2019-08-06 13:32:00 +0530

    Implement PCA proof of concept
    The code needs to be made better. This is just proof of concept
    We detected lines and get points from them and do PCA.
    Another approach could have been get get line points from image,
    i.e. only those point from image that are near the line. But I think
    even this is fine. The image point based approach can be computationally
    intensive.

--- Assignments/Assignment_1/src/main.cpp
+++ Assignments/Assignment_1/src/main.cpp
@@ -6,6 +6,7 @@
 #include "json.hpp"
 
 // cv headers
+#include "opencv2/core.hpp"
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
@@ -31,11 +32,35 @@ static int IMG_HEIGHT, IMG_WIDTH;
 //functions
 void convert_to_gray_scale(Mat &, Mat &, ColorSpace);
 void make_split_window(Mat &, Mat &, Mat &);
+
+
+void drawAxis(Mat& img, Point p, Point q, Scalar colour, const float scale = 0.2)
+{
+	double angle = atan2((double)p.y - q.y, (double)p.x - q.x); // angle in radians
+	double hypotenuse = sqrt((double)(p.y - q.y) * (p.y - q.y) + (p.x - q.x) * (p.x - q.x));
+	// Here we lengthen the arrow by a factor of scale
+	q.x = (int)(p.x - scale * hypotenuse * cos(angle));
+	q.y = (int)(p.y - scale * hypotenuse * sin(angle));
+	line(img, p, q, colour, 1, LINE_AA);
+	// create the arrow hooks
+	p.x = (int)(q.x + 9 * cos(angle + CV_PI / 4));
+	p.y = (int)(q.y + 9 * sin(angle + CV_PI / 4));
+	line(img, p, q, colour, 1, LINE_AA);
+	p.x = (int)(q.x + 9 * cos(angle - CV_PI / 4));
+	p.y = (int)(q.y + 9 * sin(angle - CV_PI / 4));
+	line(img, p, q, colour, 1, LINE_AA);
+}
 int main(int argc, char **argv)
 {
     Ptr<BackgroundSubtractor> pBackSub;
     pBackSub = createBackgroundSubtractorMOG2();
-    std::ifstream ifile("Assignments/Assignment_1/input/tuning_params.json");
+
+#ifdef _MSC_VER
+    std::ifstream ifile("C:/Projects/Acads/COL780/Assignments/Assignment_1/input/tuning_params.json");
+#else
+	std::ifstream ifile("Assignments/Assignment_1/input/tuning_params.json");
+#endif
+
     json j;
     ifile >> j;
     // declarations
@@ -101,9 +126,12 @@ int main(int argc, char **argv)
         // Hough Transform to fit line
         HoughLines(frame_bw, lines, 1, CV_PI / 180, j["hough_threshold"], 0, 0); // runs the actual detection
 
+
+		// Get Detected line points
+		std::vector<Point> line_points;
         for (size_t i = 0; i < lines.size(); i++)
         {
-            std::cout << " No of Lines " << lines.size() << "\n";
+            // std::cout << " No of Lines " << lines.size() << "\n";
             float rho = lines[i][0], theta = lines[i][1];
             Point pt1, pt2;
             double a = cos(theta), b = sin(theta);
@@ -112,9 +140,54 @@ int main(int argc, char **argv)
             pt1.y = cvRound(y0 + 500 * (a));
             pt2.x = cvRound(x0 - 500 * (-b));
             pt2.y = cvRound(y0 - 500 * (a));
+
+#ifdef DISPLAY_LINES
             line(frame, pt1, pt2, Scalar(0, 0, 255), 3, LINE_AA);
+#endif
+			// Get all line points
+			// Another approach can be get all points from the image that are near to these detected lines.
+			LineIterator it(frame_bw, pt1, pt2, 8);
+			for (int i = 0; i < it.count; i++, ++it) {
+				line_points.push_back(it.pos());
+			}
+
         }
+
+		// Compute PCA
+		vector<Point2d> eigen_vecs(2);
+		vector<double> eigen_val(2);
+
+		if (!line_points.empty()) {
+			// std::cout << line_points.size() << std::endl;
+			//Perform PCA analysis
+			Mat data_pts = Mat(line_points.size(), 2, CV_64F);
+			for (int i = 0; i < data_pts.rows; i++) {
+				data_pts.at<double>(i, 0) = line_points[i].x;
+				data_pts.at<double>(i, 1) = line_points[i].y;
+			}
+
+			PCA pca_analysis(data_pts, Mat(), PCA::DATA_AS_ROW);
+			//Store the center of the object
+			Point cntr = Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)), static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
+
+			// Store the eigenvalues and eigenvectors
+			for (int i = 0; i < 2; i++)
+			{
+				eigen_vecs[i] = Point2d(pca_analysis.eigenvectors.at<double>(i, 0), pca_analysis.eigenvectors.at<double>(i, 1));
+				// std::cout << eigen_vecs[i] << std::endl;
+				eigen_val[i] = pca_analysis.eigenvalues.at<double>(i);
+				// std::cout << eigen_val[i] << std::endl;
+			}
+
+
+			Point p1 = cntr + 0.02 * Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]), static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
+			drawAxis(frame, cntr, p1, Scalar(0, 255, 0), 1);
+		}
+
+		// Clear lines and points
+		line_points.clear();
         lines.clear();
+
         // Rendering stage
         convert_to_gray_scale(frame_bw, frame_bw, gray_to_rgb); // mandatory step so that concatenated_window_frame has same color Channel
         make_split_window(frame, frame_bw, concatenated_window_frame);

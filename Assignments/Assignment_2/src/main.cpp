7d142b0c67352730985c6c37da9796455c48ee61
ricoseeds <rghchaks73@gmail.com>
2019-09-10 03:29:27 +0530

    Compute SIFT descriptors for a single image, also find Centre of Gravity of the point cloud that arise from the sift descriptor

--- Assignments/Assignment_2/src/main.cpp
+++ Assignments/Assignment_2/src/main.cpp
@@ -1,7 +1,75 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include "json.hpp"
+#include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp> //Thanks to Alessandro
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/imgproc.hpp> // drawing the COG
 
-int main()
+using namespace cv;
+using cv::Mat;
+using cv::Ptr;
+using cv::xfeatures2d::SIFT;
+using namespace std;
+using json = nlohmann::json;
+
+// prototypes
+void get_keypoints(Mat &input, vector<KeyPoint> &kpts, Mat &desc);
+void show_keypoints(Mat &input, Mat &output, vector<KeyPoint> &kpts);
+Point2f compute_COG(vector<KeyPoint> &kpts);
+
+int main(int argc, const char *argv[])
 {
-    std::cout << "Hello";
+#ifdef _MSC_VER
+    std::ifstream ifile("C:/Projects/Acads/COL780/Assignments/Assignment_2/input/meta.json");
+#else
+    std::ifstream ifile("Assignments/Assignment_2/input/meta.json");
+#endif
+    json meta_parser;
+    ifile >> meta_parser;
+    //
+    vector<KeyPoint> kpts;
+    Mat desc;
+    Mat input = imread(meta_parser["data"][0], 0); //Load as grayscale
+    Mat output;                                    // output of sift
+    get_keypoints(input, kpts, desc);
+    imshow("img", input);
+
+    // Add results to image and save.
+    show_keypoints(input, output, kpts);
+
+    // test keypoint cood
+    cout << kpts[0].pt.x << " : " << kpts[0].pt.y;
+    Point2f cog = compute_COG(kpts);
+    cout << "\n COG " << cog.x << " : " << cog.y << "\n";
+    cv::circle(output, cog, 18, Scalar(0, 255, 0), 2);
+    imshow("img", output);
+    waitKey(0);
     return 0;
+}
+
+void get_keypoints(Mat &input, vector<KeyPoint> &kpts, Mat &desc)
+{
+    Ptr<Feature2D> sift = SIFT::create();
+    sift->detectAndCompute(input, Mat(), kpts, desc);
+}
+void show_keypoints(Mat &input, Mat &output, vector<KeyPoint> &kpts)
+{
+    drawKeypoints(input, kpts, output);
+}
+
+Point2f compute_COG(vector<KeyPoint> &kpts)
+{
+    Point2f results;
+    for (auto kpt : kpts)
+    {
+        results.x += kpt.pt.x;
+        results.y += kpt.pt.y;
+    }
+    results.x /= kpts.size();
+    results.y /= kpts.size();
+    return results;
 }

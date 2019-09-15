87b2bfcc312b8bb2fda8a36426135b13d40d8e44
ricoseeds <rghchaks73@gmail.com>
2019-09-15 05:47:38 +0530

    Minor bug fix for proper stitching

--- Assignments/Assignment_2/src/main.cpp
+++ Assignments/Assignment_2/src/main.cpp
@@ -44,40 +44,39 @@ int main(int argc, const char *argv[])
     Mat input_1 = imread(meta_parser["data"][0], 0); //Load as grayscale
     Mat input_2 = imread(meta_parser["data"][1], 0); //Load as grayscale
 
-	resize(input_1, input_1, Size(input_1.size().width / 6, input_1.size().height / 6), cv::INTER_AREA);
-	resize(input_2, input_2, Size(input_2.size().width / 6, input_2.size().height / 6), cv::INTER_AREA);
+    resize(input_1, input_1, Size(input_1.size().width / 6, input_1.size().height / 6), cv::INTER_AREA);
+    resize(input_2, input_2, Size(input_2.size().width / 6, input_2.size().height / 6), cv::INTER_AREA);
 
-    Mat output;                                      // output of sift
+    Mat output; // output of sift
     get_keypoints(input_1, kpts_image_1, desc_1);
     get_keypoints(input_2, kpts_image_2, desc_2);
-    
-	
-	// Add results to image and save.
+
+    // Add results to image and save.
     //show_keypoints(input_1, output, kpts_image_1);
-	//imshow("matches", output);
+    //imshow("matches", output);
 
     std::vector<Point2f> kpts_1;
     std::vector<Point2f> kpts_2;
-	std::vector<float> keypoints_distance;
+    std::vector<float> keypoints_distance;
 
-	// We would also create a matric of size nxn where n is number images.
-	// Then the (i,j)th element should be the normalized distanace value.
-	// This could generate a symmetrical matrix.
-	// And then in each row, we see which  element is lowest, the lowest element index will be matched image.
-	// say the lowest element is a{p,q} then pth image matches qth image best and must be stitched...
-	// Now I am stuck on what should be the order .... 
+    // We would also create a matric of size nxn where n is number images.
+    // Then the (i,j)th element should be the normalized distanace value.
+    // This could generate a symmetrical matrix.
+    // And then in each row, we see which  element is lowest, the lowest element index will be matched image.
+    // say the lowest element is a{p,q} then pth image matches qth image best and must be stitched...
+    // Now I am stuck on what should be the order ....
 
-	// Show matched keypoints
+    // Show matched keypoints
     vector<DMatch> matches;
     match(desc_1, desc_2, matches);
 
-	Mat img_matches;
-	drawMatches(input_1, kpts_image_1, input_2, kpts_image_2, matches, img_matches, Scalar::all(-1),
-		Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-	imshow("matched_image", img_matches);
+    Mat img_matches;
+    drawMatches(input_1, kpts_image_1, input_2, kpts_image_2, matches, img_matches, Scalar::all(-1),
+                Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    imshow("matched_image", img_matches);
 
     vector<char> match_mask(matches.size(), 1);
-	keypoints_distance.reserve(matches.size());
+    keypoints_distance.reserve(matches.size());
     if (static_cast<int>(match_mask.size()) < 3)
     {
         cout << "Not enough correspondence";
@@ -87,19 +86,19 @@ int main(int argc, const char *argv[])
     {
         kpts_1.push_back(kpts_image_1[matches[i].queryIdx].pt);
         kpts_2.push_back(kpts_image_2[matches[i].trainIdx].pt);
-		keypoints_distance.push_back(matches[i].distance);
+        keypoints_distance.push_back(matches[i].distance);
     }
 
-	//Matched images will have least normalized distance.
-	float normalized_distance = std::accumulate(keypoints_distance.begin(), keypoints_distance.end(), 0)/ keypoints_distance.size();
+    //Matched images will have least normalized distance.
+    float normalized_distance = std::accumulate(keypoints_distance.begin(), keypoints_distance.end(), 0) / keypoints_distance.size();
 
-	// We must find a way to keep cache this distance, then attach images based on this distance.
-	std::cout << "normalized_distance: " <<  normalized_distance << std::endl;
+    // We must find a way to keep cache this distance, then attach images based on this distance.
+    std::cout << "normalized_distance: " << normalized_distance << std::endl;
 
-	Mat H = findHomography(kpts_1, kpts_2, RANSAC);
+    Mat H = findHomography(kpts_1, kpts_2, RANSAC);
     cout << "Keypoint 1 size = " << kpts_1.size() << " Keypoint_2_size = " << kpts_2.size() << endl;
     cout << "Homography matrix  : " << H << endl;
-   /* H.at<double>(0, 2) = H.at<double>(0, 2) * -1.0;
+    /* H.at<double>(0, 2) = H.at<double>(0, 2) * -1.0;
     H.at<double>(1, 2) = H.at<double>(1, 2) * -1.0;
     int tx = H.at<double>(0, 2);
     int ty = H.at<double>(1, 2);*/
@@ -118,16 +117,16 @@ int main(int argc, const char *argv[])
     //projective_warp.copyTo(final_im(cv::Rect(0, 0, projective_warp.cols, projective_warp.rows)));
     //input_1.copyTo(final_im(cv::Rect(0, 0, input_1.cols, input_1.rows)));
 
-	int width = input_1.cols + input_2.cols;
-	int height = input_1.rows + input_2.rows;
-	
-	cv::Mat result;
-	warpPerspective(input_2, result, H, cv::Size(input_1.cols + input_2.cols, input_2.rows));
-	imshow("Result_warped", result);
+    int width = input_1.cols + input_2.cols;
+    int height = input_1.rows + input_2.rows;
 
-	cv::Mat half(result, cv::Rect(0, 0, input_1.cols, input_1.rows));
-	input_1.copyTo(half);
-	imshow("Result", result);
+    cv::Mat result;
+    warpPerspective(input_2, result, H.inv(), cv::Size(input_1.cols + input_2.cols, input_2.rows));
+    imshow("Result_warped", result);
+
+    cv::Mat half(result, cv::Rect(0, 0, input_1.cols, input_1.rows));
+    input_1.copyTo(half);
+    imshow("Result", result);
     waitKey(0);
     return 0;
 }

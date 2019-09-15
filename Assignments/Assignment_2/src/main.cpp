7c602d7d5f637e8f161ac303ff668e803e3e3a7e
Sukhraj Singh <singh.sukhraj@gmail.com>
2019-09-15 12:33:24 +0530

    Add laplacian blend method.
    THis method is for reference, it works fine for two images of same size.
    We need to change this implementation, or take idea from it to implement
    our own.

--- Assignments/Assignment_2/src/main.cpp
+++ Assignments/Assignment_2/src/main.cpp
@@ -23,9 +23,115 @@ void show_keypoints(Mat &input, Mat &output, vector<KeyPoint> &kpts);
 Point2f compute_COG(vector<KeyPoint> &kpts);
 void populate_point2f_keypoint_vector(std::vector<Point2f> &kpts_as_point2f, vector<KeyPoint> &kpts);
 inline void match(Mat &desc1, Mat &desc2, vector<DMatch> &matches);
-
+int BlendLaplacian(Mat l8u, Mat r8u);
 const double kDistanceCoef = 4.0;
 const int kMaxMatchingSize = 20;
+
+
+// Reference
+// https://www.morethantechnical.com/2011/11/13/just-a-simple-laplacian-pyramid-blender-using-opencv-wcode/
+class LaplacianBlending {
+private:
+	Mat_<Vec3f> left;
+	Mat_<Vec3f> right;
+	Mat_<float> blendMask;
+
+	vector<Mat_<Vec3f> > leftLapPyr, rightLapPyr, resultLapPyr;
+	Mat leftSmallestLevel, rightSmallestLevel, resultSmallestLevel;
+	vector<Mat_<Vec3f> > maskGaussianPyramid; //masks are 3-channels for easier multiplication with RGB
+
+	int levels;
+
+
+	void buildPyramids() {
+		buildLaplacianPyramid(left, leftLapPyr, leftSmallestLevel);
+		buildLaplacianPyramid(right, rightLapPyr, rightSmallestLevel);
+		buildGaussianPyramid();
+	}
+
+	void buildGaussianPyramid() {
+		assert(leftLapPyr.size()>0);
+
+		maskGaussianPyramid.clear();
+		Mat currentImg;
+		cvtColor(blendMask, currentImg, COLOR_GRAY2BGR);
+		maskGaussianPyramid.push_back(currentImg); //highest level
+
+		currentImg = blendMask;
+		for (int l = 1; l<levels + 1; l++) {
+			Mat _down;
+			if (leftLapPyr.size() > l) {
+				pyrDown(currentImg, _down, leftLapPyr[l].size());
+			}
+			else {
+				pyrDown(currentImg, _down, leftSmallestLevel.size()); //smallest level
+			}
+
+			Mat down;
+			cvtColor(_down, down, COLOR_GRAY2BGR);
+			maskGaussianPyramid.push_back(down);
+			currentImg = _down;
+		}
+	}
+
+	void buildLaplacianPyramid(const Mat& img, vector<Mat_<Vec3f> >& lapPyr, Mat& smallestLevel) {
+		lapPyr.clear();
+		Mat currentImg = img;
+		for (int l = 0; l<levels; l++) {
+			Mat down, up;
+			pyrDown(currentImg, down);
+			pyrUp(down, up, currentImg.size());
+			Mat lap = currentImg - up;
+			lapPyr.push_back(lap);
+			currentImg = down;
+		}
+		currentImg.copyTo(smallestLevel);
+	}
+
+	Mat_<Vec3f> reconstructImgFromLapPyramid() {
+		Mat currentImg = resultSmallestLevel;
+		for (int l = levels - 1; l >= 0; l--) {
+			Mat up;
+
+			pyrUp(currentImg, up, resultLapPyr[l].size());
+			currentImg = up + resultLapPyr[l];
+		}
+		return currentImg;
+	}
+
+	void blendLapPyrs() {
+		resultSmallestLevel = leftSmallestLevel.mul(maskGaussianPyramid.back()) +
+			rightSmallestLevel.mul(Scalar(1.0, 1.0, 1.0) - maskGaussianPyramid.back());
+		for (int l = 0; l<levels; l++) {
+			Mat A = leftLapPyr[l].mul(maskGaussianPyramid[l]);
+			Mat antiMask = Scalar(1.0, 1.0, 1.0) - maskGaussianPyramid[l];
+			Mat B = rightLapPyr[l].mul(antiMask);
+			Mat_<Vec3f> blendedLevel = A + B;
+
+			resultLapPyr.push_back(blendedLevel);
+		}
+	}
+
+public:
+	LaplacianBlending(const Mat_<Vec3f>& _left, const Mat_<Vec3f>& _right, const Mat_<float>& _blendMask, int _levels) :
+		left(_left), right(_right), blendMask(_blendMask), levels(_levels)
+	{
+		assert(_left.size() == _right.size());
+		assert(_left.size() == _blendMask.size());
+		buildPyramids();
+		blendLapPyrs();
+	};
+
+	Mat_<Vec3f> blend() {
+		return reconstructImgFromLapPyramid();
+	}
+};
+
+Mat_<Vec3f> LaplacianBlend(const Mat_<Vec3f>& l, const Mat_<Vec3f>& r, const Mat_<float>& m) {
+	LaplacianBlending lb(l, r, m, 4);
+	return lb.blend();
+}
+
 
 int main(int argc, const char *argv[])
 {
@@ -103,12 +209,29 @@ int main(int argc, const char *argv[])
     warpPerspective(input_2, result, H.inv(), cv::Size(input_1.cols + input_2.cols, input_2.rows), INTER_CUBIC);
     // imshow("Result_warped", result);
 
+	//BlendLaplacian(input_1, result);
+
     cv::Mat half(result, cv::Rect(0, 0, input_1.cols, input_1.rows));
     input_1.copyTo(half);
     imshow("Result Panorama", result);
+
+
     waitKey(0);
     return 0;
 }
+
+
+int BlendLaplacian(Mat l8u, Mat r8u) {
+	Mat_<Vec3f> l; l8u.convertTo(l, CV_32F, 1.0 / 255.0);
+	Mat_<Vec3f> r; r8u.convertTo(r, CV_32F, 1.0 / 255.0);
+
+	Mat_<float> m(l.rows, l.cols, 0.0);
+	m(Range::all(), Range(0, m.cols / 2)) = 1.0;
+
+	Mat_<Vec3f> blend = LaplacianBlend(l, r, m);
+	imshow("blended", blend);
+}
+
 
 void get_keypoints(Mat &input, vector<KeyPoint> &kpts, Mat &desc)
 {

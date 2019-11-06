2d57b43a963ba3fb4897235c5348a10f9a38536d
ricoseeds <rghchaks73@gmail.com>
2019-11-06 17:23:14 +0530

    Save

--- Assignments/Assignment_4/code.py
+++ Assignments/Assignment_4/code.py
@@ -1,170 +1,66 @@
 
 
 
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torch.optim import lr_scheduler
-# import torchvision.transforms as transforms
-# import numpy as np
-# import torchvision
-# from torchvision import datasets, models, transforms
-# # import matplotlib.pyplot as plt
-# import time
-# import os
-# import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision.transforms as transforms
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+# import matplotlib.pyplot as plt
+import time
+import os
+import copy
 
-# # plt.ion() 
-# import cv2
-# import torch.nn as nn
-# import torch.nn.functional as F
-# from torchvision import transforms
-# transform = transforms.Compose([
-# transforms.ToPILImage(),            
-# transforms.Scale((50,50)),                    
-# transforms.ToTensor()                    
-#  ])
-# classes = ['next', 'prev', 'stop']
-
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 3)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, 16, 3)
-#         self.fc1 = nn.Linear(16 * 11 * 11, 200)
-#         self.fc2 = nn.Linear(200, 40)
-#         self.fc3 = nn.Linear(40, 3)
-
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, 16 * 11 *11)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-
-
-# # cap = cv2.VideoCapture('/Users/arghachakraborty/Desktop/stop_1.mov')
-# # live camera feed
-# cap = cv2.VideoCapture('/Users/arghachakraborty/Desktop/stop_1.mov')
-# PATH = '/Users/arghachakraborty/Projects/CV_assignments/Assignments/Assignment_4/trained_net_3.pth'
-# net = Net()
-# net.load_state_dict(torch.load(PATH))
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#     img_t = transform(frame)
-#     batch_t = torch.unsqueeze(img_t, 0)
-#     net.eval()
-#     out = net(batch_t)
-#     _, index = torch.max(out, 1)
-#     percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-#     # print(classes[index[0]], percentage[index[0]].item())
-#     frame = cv2.putText(frame, classes[index[0]] + ' ' + str(percentage[index[0]].item()), (50,50), cv2.FONT_HERSHEY_SIMPLEX ,  
-#                    1, (0,255,0), 2, cv2.LINE_AA) 
-#     cv2.imshow('frame',frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
-# print("test")
-# *((*((*(*(*(*(*(*((*(*(*(*))))))))))))))
-# Required moduls
+# plt.ion() 
 import cv2
-import numpy
-# from pykeyboard import PyKeyboard
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+transform = transforms.Compose([
+transforms.ToPILImage(),            
+transforms.Scale((50,50)),                    
+transforms.ToTensor()                    
+ ])
+classes = ['next', 'prev', 'stop']
 
-# Constants for finding range of skin color in YCrCb
-# min_YCrCb = numpy.array([0,96,107],numpy.uint8)
-# max_YCrCb = numpy.array([134,255,123],numpy.uint8)
-min_YCrCb = numpy.array([0,96,103],numpy.uint8)
-max_YCrCb = numpy.array([134,255,123],numpy.uint8)
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 11 * 11, 200)
+        self.fc2 = nn.Linear(200, 40)
+        self.fc3 = nn.Linear(40, 3)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-gameflag = False
-# keyboard = PyKeyboard()
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 11 *11)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+cap = cv2.VideoCapture('/Users/arghachakraborty/Desktop/test.mov')
+PATH = '/Users/arghachakraborty/Projects/CV_assignments/Assignments/Assignment_4/trained_net_3.pth'
+net = Net()
+net.load_state_dict(torch.load(PATH))
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    img_t = transform(frame)
+    batch_t = torch.unsqueeze(img_t, 0)
+    net.eval()
+    out = net(batch_t)
+    _, index = torch.max(out, 1)
+    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    frame = cv2.putText(frame, classes[index[0]] + ' ' + str(percentage[index[0]].item()), (50,50), cv2.FONT_HERSHEY_SIMPLEX ,  
+                   1, (0,255,0), 2, cv2.LINE_AA) 
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# Create a window to display the camera feed
-cv2.namedWindow('Camera Output')
-
-# Get pointer to video frames from primary device
-videoFrame = cv2.VideoCapture(0)
-# videoFrame = cv2.VideoCapture('/Users/arghachakraborty/Downloads/sukhraj_train_vids/stop.mp4')
-
-while True: # any key pressed has a value >= 0
-
-    # Grab video frame, decode it and return next video frame
-    readSucsess, sourceImage = videoFrame.read()
-    sourceImage = cv2.flip(sourceImage, 1)
-
-    # Convert image to YCrCb
-    imageYCrCb = cv2.cvtColor(sourceImage,cv2.COLOR_BGR2YCR_CB)
-
-    # Find region with skin tone in YCrCb image
-    skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-    
-    skin = cv2.bitwise_and(sourceImage, sourceImage, mask = skinRegion)
-
-    # Do contour detection on skin region
-    contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.rectangle(skin,(0,300),(640,200),(0,0,255),3)
-    # cv2.drawContours(skin, contours, -1, (0,255,0), 3)
-    # Display the source image
-    final = cv2.medianBlur(skin, 7)
-    cv2.imshow('Camera Output',final)
-    # cv2.imwrite('test.jpeg', skin)
-
-    # Check for user input to close program
-    if cv2.waitKey(5) == 27: # wait 5 millisecond in each iteration of while loop
-        break 
-
-# Close window and camera after exiting the while loop
-cv2.destroyWindow('Camera Output')
-videoFrame.release()
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-# def nothing(x):
-#   pass
-# cv2.namedWindow('Colorbars')
-# hh='Max'
-# hl='Min'
-# wnd = 'Colorbars'
-# cv2.createTrackbar("Min_y", "Colorbars",0,255,nothing)
-# cv2.createTrackbar("Min_cb", "Colorbars",0,255,nothing)
-# cv2.createTrackbar("Min_cr", "Colorbars",0,255,nothing)
-# cv2.createTrackbar("Max_y", "Colorbars",0,255,nothing)
-# cv2.createTrackbar("Max_cb", "Colorbars",0,255,nothing)
-# cv2.createTrackbar("Max_cr", "Colorbars",0,255,nothing)
-# img = cv2.imread('test.jpeg',1)
-# img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-# cv2.imshow(' Output',img)
-
-# while(1):
-#    miny=cv2.getTrackbarPos("Min_y", "Colorbars")
-#    mincb=cv2.getTrackbarPos("Min_cb", "Colorbars")
-#    mincr=cv2.getTrackbarPos("Min_cr", "Colorbars")   
-#    maxy=cv2.getTrackbarPos("Max_y", "Colorbars")
-#    maxcb=cv2.getTrackbarPos("Max_cb", "Colorbars")
-#    maxcr=cv2.getTrackbarPos("Max_cr", "Colorbars")
-#    print("miny" + str(miny))
-#    print("mincb" + str(mincb))
-#    print("mincr" + str(mincr))
-#    print("maxy" + str(maxy))
-#    print("maxcb" + str(maxcb))
-#    print("maxcr" + str(maxcr))
-#    imageYCrCb = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
-#    min_YCrCb = np.array([miny,mincb,mincr],np.uint8)
-#    max_YCrCb = np.array([maxy,maxcb,maxcr],np.uint8)
-#     # Find region with skin tone in YCrCb image
-#    skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-#    skin = cv2.bitwise_and(img, img, mask = skinRegion)
-#    cv2.imshow('Camera Output',skin)
-#    k = cv2.waitKey(1) & 0xFF
-#    if k == ord('m'):
-#      mode = not mode
-#    elif k == 27:
-#      break
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
--- Assignments/Assignment_4/finger.py
+++ Assignments/Assignment_4/finger.py
@@ -2,95 +2,112 @@ import cv2
 import numpy as np
 import copy
 import math
-#from appscript import app
+import os
 
-# Environment:
-# OS    : Mac OS EL Capitan
-# python: 3.5
-# opencv: 2.4.13
-
-# parameters
 cap_region_x_begin=0.5  # start point/total width
 cap_region_y_end=0.8  # start point/total width
 threshold = 60  #  BINARY threshold
 blurValue = 41  # GaussianBlur parameter
 bgSubThreshold = 50
 learningRate = 0
-
+ 
 # variables
 isBgCaptured = 0   # bool, whether the background captured
 triggerSwitch = False  # if true, keyborad simulator works
-
-def printThreshold(thr):
-    print("! Changed threshold to "+str(thr))
+# prevSwitch = False
+# stopSwitch = False
+# nextSwitch = False
+prev_1 = '/Users/arghachakraborty/Projects/CV_assignments/data/train/prev_2/'
+stop_1 = '/Users/arghachakraborty/Projects/CV_assignments/data/train/stop_2/'
+next_1 = '/Users/arghachakraborty/Projects/CV_assignments/data/train/next_2/'
+# count_prv = 0
+# count_nxt = 0
+# count_stp = 0
+# count_prv = 0
+# count_nxt = 0
+# count_stp = 0
+prevSwitch = False
+stopSwitch = False
+nextSwitch = False
+ 
+def count_files(in_directory):
+    joiner= (in_directory + os.path.sep).__add__
+    return sum(
+        os.path.isfile(filename)
+        for filename
+        in map(joiner, os.listdir(in_directory))
+    )
 
 
 def removeBG(frame):
     fgmask = bgModel.apply(frame,learningRate=learningRate)
     # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     # res = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-
+ 
     kernel = np.ones((3, 3), np.uint8)
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
+ 
+def clear_all_switch():
+    prevSwitch = False
+    stopSwitch = False
+    nextSwitch = False
+ 
+def capture_frame(frame):
+    global count_prv, count_stp, count_nxt, prevSwitch, stopSwitch, nextSwitch
+    frame = cv2.resize(frame, (50, 50), interpolation = cv2.INTER_AREA)
+    if prevSwitch == True:
+        count_prv = count_prv + 1
+        cv2.imwrite(prev_1+str(count_prv)+".jpeg", frame)
+        print("prev "+ str(count_prv))
+    elif stopSwitch == True:
+        count_stp = count_stp + 1
+        cv2.imwrite(stop_1+str(count_stp)+".jpeg", frame)
+        print("stop "+ str(count_stp))
+    elif nextSwitch == True:
+        count_nxt = count_nxt + 1
+        cv2.imwrite(next_1+str(count_nxt)+".jpeg", frame)
+        print("next "+ str(count_nxt))
 
-
-def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
-    #  convexity defect
-    hull = cv2.convexHull(res, returnPoints=False)
-    if len(hull) > 3:
-        defects = cv2.convexityDefects(res, hull)
-        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
-
-            cnt = 0
-            for i in range(defects.shape[0]):  # calculate the angle
-                s, e, f, d = defects[i][0]
-                start = tuple(res[s][0])
-                end = tuple(res[e][0])
-                far = tuple(res[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    cnt += 1
-                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-            return True, cnt
-    return False, 0
-
-
+ 
 # Camera
 camera = cv2.VideoCapture(0)
 camera.set(10,200)
-cv2.namedWindow('trackbar')
-cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
-
-
+# cv2.namedWindow('trackbar')
+# cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+count_prv = count_files(prev_1)
+count_nxt = count_files(next_1)
+count_stp = count_files(stop_1)
 while camera.isOpened():
     ret, frame = camera.read()
-    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
+    # threshold = cv2.getTrackbarPos('trh1', 'trackbar')
     frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
     frame = cv2.flip(frame, 1)  # flip the frame horizontally
     cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
     cv2.imshow('original', frame)
-
+ 
     #  Main operation
     if isBgCaptured == 1:  # this part wont run until background captured
         img = removeBG(frame)
+        frame_ = copy.deepcopy(frame)
         img = img[0:int(cap_region_y_end * frame.shape[0]),
                     int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
-        cv2.imshow('mask', img)
-
+        frame_ = frame_[0:int(cap_region_y_end * frame.shape[0]),
+                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]] 
+        # cv2.imshow('mask', img)
+ 
         # convert the image into binary image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
-        cv2.imshow('blur', blur)
+        # cv2.imshow('blur', blur)
         ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
         cv2.imshow('ori', thresh)
-
-
+        if prevSwitch or stopSwitch or nextSwitch :
+            capture_frame(thresh)
+ 
+ 
         # get the coutours
         thresh1 = copy.deepcopy(thresh)
         contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -103,22 +120,22 @@ while camera.isOpened():
                 if area > maxArea:
                     maxArea = area
                     ci = i
-
+ 
             res = contours[ci]
             hull = cv2.convexHull(res)
             drawing = np.zeros(img.shape, np.uint8)
             cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
             # cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
-
+ 
             # isFinishCal,cnt = calculateFingers(res,drawing)
             # if triggerSwitch is True:
             #     if isFinishCal is True and cnt <= 2:
             #         print (cnt)
                     #app('System Events').keystroke(' ')  # simulate pressing blank space
-                    
-
-        cv2.imshow('output', drawing)
-
+                   
+ 
+        # cv2.imshow('output', drawing)
+ 
     # Keyboard OP
     k = cv2.waitKey(10)
     if k == 27:  # press ESC to exit
@@ -133,7 +150,25 @@ while camera.isOpened():
         bgModel = None
         triggerSwitch = False
         isBgCaptured = 0
+        clear_all_switch()
         print ('!!!Reset BackGround!!!')
     elif k == ord('n'):
         triggerSwitch = True
         print ('!!!Trigger On!!!')
+    elif k == ord('u'):
+        clear_all_switch()
+        prevSwitch = True
+        print ('Anotating prev')
+    elif k == ord('i'):
+        clear_all_switch()
+        stopSwitch = True
+        print ('Anotating stop')
+    elif k == ord('o'):
+        clear_all_switch()
+        nextSwitch = True
+        print ('Anotating next')
+    elif k == ord('p'):
+        clear_all_switch()
+        print ('clearall')
+
+# print(count_files(stop_1))
